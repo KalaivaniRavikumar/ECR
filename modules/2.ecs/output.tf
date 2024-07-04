76ea8cdfc33fd119c05a3c025f061aed610bf59b
KalaivaniRavikumar <kalaivaniravikumarbox@gmail.com>
2024-07-04 12:30:05 -0400

    Add files via upload

---
+++ modules/2.ecs/output.tf
@@ -0,0 +1,26 @@
+output "vpc_cidr_block" {
+    value = aws_vpc.myvpc
+}
+
+output "vpc_id" {
+  value = aws_vpc.myvpc.id
+}
+
+output "public_subnet_id_1" {
+  value = aws_subnet.public_subnet_1_cidr.id
+}
+
+output "public_subnet_id_2" {
+  value = aws_subnet.public_subnet_2_cidr.id
+}
+
+output "private_subnet_id_1" {
+  value = aws_subnet.private_subnet_1_cidr.id
+} 
+output "private_subnet_id_2" {
+  value = aws_subnet.private_subnet_2_cidr.id
+} 
+
+output "private_sn" {
+  value = local.private_sn
+}
